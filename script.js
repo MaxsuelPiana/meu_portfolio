e4ae7ea644e59ac534b3e788d84e3199bd412b7d
MaxsuelPiana <maxsuelpiana1097@gmail.com>
2025-05-03 10:02:34 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+// Futuramente: pode-se adicionar scroll automático nos rovers, ou interações no cubo
+console.log("Portfólio de Maxsuel Piana carregado!");
+
+window.addEventListener('scroll', function() {
+    const contatosSection = document.getElementById('contatos');
+    const rect = contatosSection.getBoundingClientRect();
+});
+    
